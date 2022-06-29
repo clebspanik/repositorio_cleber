eb6cc704c2fd5929ed2b02fc3d0a103321d20b70
clebspanik <cleberpanik@gmail.com>
2022-06-29 11:24:36 -0300

    TDD
    Teste - Adicionar triângulo e retângulo - Aula 29/06/2022

---
+++ TDDTesteUnitario/app/src/main/java/cleber/roberto/uno/tddtesteunitario/Forma/Retangulo.java
@@ -0,0 +1,14 @@
+package cleber.roberto.uno.tddtesteunitario.Forma;
+
+public class Retangulo extends Forma {
+
+
+    public Retangulo(int numMedidas) {
+        super(numMedidas);
+    }
+
+    @Override
+    public double area() {
+        return 0;
+    }
+}
---
+++ TDDTesteUnitario/app/src/test/java/cleber/roberto/uno/tddtesteunitario/RetanguloTest.java
@@ -0,0 +1,27 @@
+package cleber.roberto.uno.tddtesteunitario;
+
+
+import static org.junit.Assert.*;
+import static java.lang.Math.PI;
+
+import java.lang.Math;
+import org.junit.Test;
+import org.junit.experimental.theories.suppliers.TestedOn;
+import cleber.roberto.uno.tddtesteunitario.Forma.Circunferencia;
+
+
+
+public class RetanguloTest {
+
+    @Test
+    public void deveriaCalcularAreaPerimetrodeUmRetangulo () {
+
+
+    }
+
+
+
+
+}//Fim escopo;
+
+
