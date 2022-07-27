9cf1ec9e530aaabe944e1e577991531430bd7f67
clebspanik <cleberpanik@gmail.com>
2022-07-27 10:16:03 -0300

    Aula 27/07
    Alterações Android Studio

--- ConceitosMOR/app/src/main/java/desenv/ti/conceitosmor/ui/CadastroProvaActivity.java
+++ ConceitosMOR/app/src/main/java/desenv/ti/conceitosmor/ui/CadastroProvaActivity.java
@@ -1,4 +1,25 @@
 package desenv.ti.conceitosmor.ui;
 
-public class CadastroProvaActivity {
+import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import desenv.ti.conceitosmor.R;
+
+public class CadastroProvaActivity extends AppCompatActivity {
+
+    private ImageButton imageButtonIdProva;
+    private Spinner spinnerIdProva;
+
+
+    protected void OnCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cadastro_prova_activity);
+
+        imageButtonIdProva = findViewById(R.id.imageButtonIdProva);
+        spinnerIdProva     = findViewById(R.id.spinnerIdProva);
+    }
+
 }
